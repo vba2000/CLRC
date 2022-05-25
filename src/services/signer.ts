6b2320eaf32dab980c7b6c43067330df88f9d84f
Boris Volkov <bvolkov@web3tech.ru>
2022-05-25 16:18:07 +0300

    Fix build

--- src/services/signer.ts
+++ src/services/signer.ts
@@ -1,26 +1,37 @@
 import { Signer } from '@waves/signer';
-import { ProviderLedger } from '@waves/provider-ledger';
-import { ProviderKeeper } from '@waves/provider-keeper';
-import { ProviderCloud } from '@waves.exchange/provider-cloud';
-import { ProviderWeb } from '@waves.exchange/provider-web';
 import {useCallback, useEffect, useState} from "preact/hooks";
 
+let ProviderLedger: any;
+let ProviderKeeper: any;
+let ProviderCloud: any;
+let ProviderWeb: any;
 
-const PROVIDERS = {
+
+let PROVIDERS = {
     LEDGER: ProviderLedger,
     KEEPER: ProviderKeeper,
     MAIL: ProviderCloud,
     WX: ProviderWeb
 };
 
+if (typeof window !== "undefined")  {
+    ProviderLedger = require('@waves/provider-ledger').ProviderLedger;
+    ProviderKeeper = require('@waves/provider-keeper').ProviderKeeper;
+    ProviderCloud = require('@waves.exchange/provider-cloud').ProviderCloud;
+    ProviderWeb = require('@waves.exchange/provider-web').ProviderWeb;
 
+    PROVIDERS.LEDGER = ProviderLedger;
+    PROVIDERS.KEEPER = ProviderKeeper;
+    PROVIDERS.MAIL = ProviderCloud;
+    PROVIDERS.WX = ProviderWeb;
+}
 
 export const useSigner = (settings: { NODE: string }) => {
 
-    const [signer, setSigner] = useState<Signer|null>(null);
+    const [signer, setSigner] = useState<Signer | null>(null);
 
     useEffect(() => {
-        setSigner(new Signer({ NODE_URL: settings.NODE }))
+        setSigner(new Signer({NODE_URL: settings.NODE}))
     }, [settings.NODE]);
 
     const selectProvider = useCallback(async (provider: keyof typeof PROVIDERS) => {
@@ -36,4 +47,4 @@ export const useSigner = (settings: { NODE: string }) => {
         signer,
         selectProvider
     }
-}
+};
